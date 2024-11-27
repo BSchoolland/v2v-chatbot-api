4a74abf52667698ca9d9984f2f5c6e9fa8cc763c
Benjamin Schoolland <bschoolland@gmail.com>
2024-11-27 00:17:39 -0800

    feat: basic testing route

--- api/readPageContent.js
+++ api/readPageContent.js
@@ -1,9 +1,13 @@
 const {getPageByUrl} = require('./database.js');
-
+const defaultPath = "https://beanythingmuseum.org";
 async function readPageContent(params) {
     // convert params to json
     params = JSON.parse(params);
     let path = params.path;
+    // if the path does not begin with http, add the default path
+    if (!path.startsWith("http")) {
+        path = defaultPath + path;
+    }
     // if there's no / at the end add it
     if (path[path.length - 1] !== "/") {
         path = path + "/";
