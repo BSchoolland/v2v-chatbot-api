241fb32593255a1455f93862195134247d233383
Benjamin Schoolland <bschoolland@gmail.com>
2024-12-31 19:14:56 -0800

    feat: implement backend payment processing

--- website-api/routes.js
+++ website-api/routes.js
@@ -5,12 +5,13 @@ const userRoutes = require('./users.js');
 const dashboardRoutes = require('./dashboard.js');
 const plansRoutes = require('./plans.js');
 const chatbotSetupRoutes = require('./chatbot-setup.js');
+const paymentRoutes = require('./payments.js');
 
 router.use('', userRoutes);
 router.use('', dashboardRoutes);
 router.use('', plansRoutes);
 router.use('/chatbot-setup', chatbotSetupRoutes);
-
+router.use('/payments', paymentRoutes);
 
 // Use cookie-parser middleware
 module.exports = router;
