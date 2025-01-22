d8f7249bd576b6dc0ada435a65d568e971ee5758
Benjamin Schoolland <bschoolland@gmail.com>
2025-01-21 17:19:44 -0800

    feat: implement model selection for chatbots
    - Add model selection UI to chatbot edit page
    
    - Update database functions to use promisified versions
    
    - Add API endpoints for model selection and updating
    
    - Ensure default model assignment works correctly

--- database/models.js
+++ database/models.js
@@ -1,107 +1,62 @@
-const { db } = require('./database.js');
+const { dbGet, dbRun, dbAll } = require('./database.js');
 
 // Add new model
 function addModel(maxContext, name, description, apiString, messageCost) {
-    return new Promise((resolve, reject) => {
-        db.run(
-            `INSERT INTO models (max_context, name, description, api_string, message_cost) VALUES (?, ?, ?, ?, ?)`,
-            [maxContext, name, description, apiString, messageCost],
-            function (err) {
-                if (err) reject(err);
-                else resolve(this.lastID);
-            }
-        );
-    });
+    return dbRun(
+        `INSERT INTO models (max_context, name, description, api_string, message_cost) VALUES (?, ?, ?, ?, ?)`,
+        [maxContext, name, description, apiString, messageCost]
+    );
 }
 
-// Retrieve model information
+// Get model by ID
 function getModelById(modelId) {
-    return new Promise((resolve, reject) => {
-        db.get(
-            `SELECT * FROM models WHERE model_id = ?`,
-            [modelId],
-            (err, row) => {
-                if (err) reject(err);
-                else resolve(row);
-            }
-        );
-    });
+    return dbGet(
+        `SELECT * FROM models WHERE model_id = ?`,
+        [modelId]
+    );
 }
 
 // Get model by name
 function getModelByName(name) {
-    return new Promise((resolve, reject) => {
-        db.get(
-            `SELECT * FROM models WHERE name = ?`,
-            [name],
-            (err, row) => {
-                if (err) reject(err);
-                else resolve(row);
-            }
-        );
-    });
+    return dbGet(
+        `SELECT * FROM models WHERE name = ?`,
+        [name]
+    );
 }
 
-// Get all available models for a plan type
+// Get available models for plan type
 function getAvailableModelsForPlanType(planTypeId) {
-    return new Promise((resolve, reject) => {
-        db.all(
-            `SELECT m.* 
-             FROM models m
-             JOIN plan_type_model ptm ON m.model_id = ptm.model_id
-             WHERE ptm.plan_type_id = ?
-             ORDER BY m.message_cost ASC`,
-            [planTypeId],
-            (err, rows) => {
-                if (err) reject(err);
-                else resolve(rows);
-            }
-        );
-    });
+    return dbAll(
+        `SELECT m.* FROM models m
+        INNER JOIN plan_type_model ptm ON m.model_id = ptm.model_id
+        WHERE ptm.plan_type_id = ?`,
+        [planTypeId]
+    );
 }
 
-// Validate if a model is available for a plan type
+// Check if model is available for plan type
 function isModelAvailableForPlanType(modelId, planTypeId) {
-    return new Promise((resolve, reject) => {
-        db.get(
-            `SELECT 1 
-             FROM plan_type_model 
-             WHERE model_id = ? AND plan_type_id = ?`,
-            [modelId, planTypeId],
-            (err, row) => {
-                if (err) reject(err);
-                else resolve(!!row);
-            }
-        );
-    });
+    return dbGet(
+        `SELECT 1 FROM plan_type_model 
+        WHERE model_id = ? AND plan_type_id = ?`,
+        [modelId, planTypeId]
+    ).then(row => !!row);
 }
 
 // Get the default model
 function getDefaultModel() {
-    return new Promise((resolve, reject) => {
-        db.get(
-            `SELECT * FROM models WHERE name = ?`,
-            ['default'],
-            (err, row) => {
-                if (err) reject(err);
-                else resolve(row);
-            }
-        );
-    });
+    return dbGet(
+        `SELECT * FROM models WHERE name = ?`,
+        ['default']
+    );
 }
 
 // Delete a model
 function deleteModel(modelId) {
-    return new Promise((resolve, reject) => {
-        db.run(
-            `DELETE FROM models WHERE model_id = ?`,
-            [modelId],
-            function (err) {
-                if (err) reject(err);
-                else resolve();
-            }
-        );
-    });
+    return dbRun(
+        `DELETE FROM models WHERE model_id = ?`,
+        [modelId]
+    );
 }
 
 module.exports = {
